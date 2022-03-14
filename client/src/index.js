63c52b87f14226be60e85f952abb4ca6340b241e
yushaer <yushaefraza@gmail.com>
2022-03-14 17:16:32 -0400

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -20,7 +20,8 @@ import WatchList from './components/WatchList';
 import Register from './routes/Register';
 
 import Login from './routes/Login';
-const store =createStore(reducers,{},compose( applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store =createStore(reducers,{},compose( applyMiddleware(thunk)));
 ReactDOM.render(
  <Provider store={store} >  
 <App/>
