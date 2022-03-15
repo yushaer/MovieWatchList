021c98a68d631c18e891914a20cc1ff39eeb43b8
yushaer <yushaefraza@gmail.com>
2022-03-15 15:27:04 -0400

    uploaded backend to heroku

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
 app.use('/',rootRouter);
-const CONNECTION_URL = process.env.db;
+const CONNECTION_URL = process.env.DB;
 const port = process.env.PORT||5000;
 mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=>app.listen(port,()=>console.log(`Server running on port: ${port}`)))
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,5 +3,8 @@ import userRouter from './users.js';
 import movieRouter from './movies.js';
 const rootRouter = express.Router();
 rootRouter.use('/movies',movieRouter);
+rootRouter.get('/',(req,res)=>{
+    res.status(200).json({"message":"You have accessed rest-api for Movie Watch List made by Yushae Raza"})
+});
 rootRouter.use('/user' ,userRouter);
 export default rootRouter;
