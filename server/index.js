47944de46dc72ba14fa619986357f268d33301d4
yushaer <yushaefraza@gmail.com>
2022-03-15 13:42:57 -0400

    added environment variable support

--- server/index.js
+++ server/index.js
@@ -1,15 +1,21 @@
 import  express from "express";
+import dotenv  from "dotenv";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from 'cors';
 import rootRouter from './routes/index.js';
+if(!process.env.PRODUCTION){
+   
+    dotenv.config()
+}
+
 const app = express();
 
 app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
 app.use('/',rootRouter);
-const CONNECTION_URL = 'mongodb+srv://YushaeR:yus123@cluster0.oxvtu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.db;
 const port = process.env.PORT||5000;
 mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=>app.listen(port,()=>console.log(`Server running on port: ${port}`)))
--- server/movie-config.js
+++ server/movie-config.js
@@ -1,11 +1,17 @@
-  const config={api_key:"2f556831a7a3e6aee32d32d69d00089a",
+ 
+ import dotenv  from "dotenv";
+ if(!process.env.PRODUCTION){
+   
+  dotenv.config()
+}
+ const config={api_key:process.env.MOVIEAPIKEY,
   url:{
     base_url:"https://api.themoviedb.org/3/movie/",
-    featured:"https://api.themoviedb.org/3/discover/movie?sort_by=popularity.asc?&api_key=2f556831a7a3e6aee32d32d69d00089a",
-    search:"https://api.themoviedb.org/3/search/movie?api_key=2f556831a7a3e6aee32d32d69d00089a&language=en-US&query=",
+    featured:"https://api.themoviedb.org/3/discover/movie?sort_by=popularity.asc?&api_key=" +process.env.MOVIEAPIKEY,
+    search:`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIEAPIKEY}&language=en-US&query=`,
     imageUrl:"https://image.tmdb.org/t/p/w185",
-    popular:"https://api.themoviedb.org/3/movie/popular?api_key=2f556831a7a3e6aee32d32d69d00089a"
+    popular:`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.MOVIEAPIKEY}`
   },
-  token_secret:"sadad"
+  token_secret:process.env.TOKENSECRET
 }
 export default config
